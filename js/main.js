c85cfa5fbefe15979574e5a94cb0c04287efb499
kido <skido627@gmail.com>
2024-10-24 13:21:01 +0900

    10/24分

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", function () {
+  var pageTopBtn = document.querySelector(".page-top");
+  pageTopBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", function () {
+    // window.pageYOffset 現在の位置を取得
+    if (window.pageYOffset > 700) {
+      pageTopBtn.classList.add("is-visible");
+    } else {
+      pageTopBtn.classList.remove("is-visible");
+    }
+  });
+});
