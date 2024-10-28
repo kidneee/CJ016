36b41adf7af93193e009f7cc7297e3a17c2963bd
kido <skido627@gmail.com>
2024-10-28 14:50:03 +0900

    10/28分

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// ハンバーガーメニュー
+const hamburgerButton = document.querySelector('#js-buttonHamburger');
+hamburgerButton.addEventListener('click', (e) => {
+  const isExpanded = e.currentTarget.getAttribute('aria-expanded') !== 'false';
+  e.currentTarget.setAttribute('aria-expanded', !isExpanded);
+  document.documentElement.classList.toggle('is-drawerActive');
+});
+
+document.getElementById('js-buttonHamburger').addEventListener('click', function () {
+  const drawer = document.getElementById('drawerMenu');
+  if (drawer.classList.contains('open')) {
+    drawer.classList.remove('open');
+  } else {
+    drawer.classList.add('open');
+  }
+});
+
+//TOPに戻る
 window.addEventListener("DOMContentLoaded", function () {
   var pageTopBtn = document.querySelector(".page-top");
   pageTopBtn.addEventListener("click", function (event) {
